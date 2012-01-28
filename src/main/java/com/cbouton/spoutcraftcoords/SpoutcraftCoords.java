9ab6d03f7daf0e43907a5c8be6f4bb0894cf67df
infina <cbouton1@gmail.com>
2012-01-27 21:21:23 -0500

    initial commit

---
+++ src/main/java/com/cbouton/spoutcraftcoords/SpoutcraftCoords.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cbouton.spoutcraftcoords;
+
+import org.spoutcraft.spoutcraftapi.addon.java.JavaAddon;
+
+/**
+ *
+ * @author Corey
+ */
+public class SpoutcraftCoords extends JavaAddon {
+    
+
+    @Override
+    public void onEnable() {
+        getClient().;
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+    
+}
